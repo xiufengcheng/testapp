6831598bd9f35c99ecb0f7fe306575a3818c51c4
程秀峰 <32472889@qq.com>
2018-09-09 18:56:14 +0800

    第二次提交

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace testapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Console.WriteLine("this is my first line");
+            Console.WriteLine("this is my second line");
         }
     }
 }
